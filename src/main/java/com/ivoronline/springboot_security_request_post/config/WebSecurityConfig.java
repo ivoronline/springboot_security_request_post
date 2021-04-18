cabc64a333e1a07f5d694643bb4e51a1a62a3f4b
igoreta <ivoronline@gmail.com>
2021-04-19 00:16:37 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_request_post/config/WebSecurityConfig.java
+++ src/main/java/com/ivoronline/springboot_security_request_post/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,9 +12,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  //=================================================================
+  // AUTHENTICATION MANAGER BEAN
+  //=================================================================
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
   //=================================================================
   // USER DETAILS SERVICE
@@ -24,12 +31,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected UserDetailsService userDetailsService() {
 
+    //CREATE USER
     UserDetails user = User.withDefaultPasswordEncoder()
       .username("myuser")
       .password("mypassword")
       .roles   ("USER")
       .build();
 
+    //STORE USER
     return new InMemoryUserDetailsManager(user);
 
   }
@@ -39,17 +48,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   //=================================================================
   @Override
   protected void configure(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity.authorizeRequests().antMatchers("/MyLogin", "/Authenticate").permitAll(); //ANONYMOUS ACCESS (NO LOGIN)
-    httpSecurity.csrf().disable();
-  }
 
-  //=================================================================
-  // AUTHENTICATION MANAGER BEAN
-  //=================================================================
-  @Bean
-  @Override
-  public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+    //ANONYMOUS ACCESS (NO LOGIN)
+    httpSecurity.authorizeRequests().antMatchers("/MyLogin", "/Authenticate").permitAll();
+
+    //ENABLE POST TO Authenticate
+    httpSecurity.csrf().disable();
+
   }
 
 }
--- src/main/java/com/ivoronline/springboot_security_request_post/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_request_post/controllers/MyController.java
@@ -2,11 +2,11 @@ package com.ivoronline.springboot_security_request_post.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
@@ -17,31 +17,44 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MyController {
 
   @Autowired private AuthenticationManager authenticationManager;
-  @Autowired private UserDetailsService    userDetailsService;
 
+  //========================================================================
+  // MY LOGIN
+  //========================================================================
   @RequestMapping("/MyLogin")
   public String myLogin() {
     return "MyLogin";
   }
 
+  //========================================================================
+  // AUTHENTICATE
+  //========================================================================
   @ResponseBody
   @PostMapping("/Authenticate")
   public String authenticate(@RequestParam String username, @RequestParam String password) {
 
-    //CREATE TOKEN (FROM USERNAME & PASSWORD)
-    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+    //CREATE AUTHENTICATION OBJECT (with Entered Username & Password)
+    Authentication authentication = new UsernamePasswordAuthenticationToken(username, password);
 
-    //AUTHENTICATE
-    try                               { authenticationManager.authenticate(authToken); }
-    catch (BadCredentialsException e) { return "Invalid Credentials";                  }
+    //GET    AUTHENTICATION OBJECT (with Authorities)
+    authentication = authenticationManager.authenticate(authentication);
 
-    //GET USER OBJECT
-    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-    System.out.println(userDetails);
+    //STORE  AUTHENTICATION OBJECT (into Context)
+    SecurityContextHolder.getContext().setAuthentication(authentication);
 
-    //SUCCESSFUL AUTHENTICATION
+    //RETURN SOMETHING
     return "User Authenticated";
 
+  }
+
+  //========================================================================
+  // HELLO
+  //========================================================================
+  @ResponseBody
+  @Secured("ROLE_USER")
+  @RequestMapping("/Hello")
+  public String hello() {
+    return "Hello from Controller";
   }
 
 }
