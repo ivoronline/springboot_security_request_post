05b8fb4503f16985ca3a31801dce4c7a0d2514d5
igoreta <ivoronline@gmail.com>
2021-04-19 08:43:12 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_security_request_post/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_security_request_post/controllers/MyController.java
@@ -1,13 +1,11 @@
 package com.ivoronline.springboot_security_request_post.controllers;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
